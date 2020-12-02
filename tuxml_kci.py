a7fde3a829c5367c258594935dbc66b69a14a688
Nusky173 <simon50.lecordier@gmail.com>
2020-12-02 16:17:34 +0100

    build un kernel à partir de kernelci avec tuxml.

--- tuxml_kci.py
+++ tuxml_kci.py
@@ -52,6 +52,15 @@ def download_kernel(args):
             tar.close()
             subprocess.call("mv linux-"+args+ " kernel", shell = True)
 
+#The function that will build the kernel with the .config or a randconfig
+#suppos that you  have already do the step 0, step1 and step2 of the how to build kernel with kernel_ci
+#and import everything you have to import to use those command
+def kernel(config):
+    current = os.getcwd()
+    print(current)
+    os.chdir("../kernelci-core")
+    print(os.getcwd()+"\n")
+    subprocess.run(args="python3 kci_build build_kernel --arch=x86_64 --build-env=gcc-8 --kdir="+current+"/kernel/ --verbose", shell=True, check=True)
 
 if __name__ == "__main__":
     args = parser()
@@ -63,19 +72,23 @@ if __name__ == "__main__":
         download_kernel(kv)
 #marker 2 done (telecharger et decompresser mon archive kernel dans mon path courant)
 
+    subprocess.call('mkdir build', shell=True)
 
     if config == 'randconfig':
         current = os.getcwd()
         os.chdir("kernel")
-        config = subprocess.call('make randconfig', shell=True)
+        subprocess.call('make randconfig', shell=True)
+        subprocess.call('mv .config ./build', shell=True)
+        config = ".config"
         os.chdir(current)
 
 #marker 3 done(générer un randconfig si pas de .config passer sinon le .config reste dans config)
     else : 
         path_config = os.getcwd()
-        subprocess.call("mv "+path_config+"/"+config +" ./kernel", shell=True)
-
-    #subprocess.call('build_kernel ' + config + ' x86_64 ' + c + ' ' + path + '/linux-' + kv,shell=True)
+        subprocess.call("mv "+path_config+"/"+config +" ./kernel/build", shell=True)
+    print(config)
+    kernel(config)
+ 
 #marker 5 done(on lance le build du kernel)
 
 #reste a prendre les outputs
