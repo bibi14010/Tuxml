107b985804df4ac2a44d2d5ab29c3f29cd3802ce
Khaled <16012048@istic.univ-rennes1.fr>
2021-03-17 12:08:51 +0100

    - build update
    added elf reading function -> it updates the bmeta.json with new info about the kernel

--- tuxml_kci.py
+++ tuxml_kci.py
@@ -15,6 +15,76 @@ import stat
 import platform
 
 import kernelci
+import elftools.elf.constants as elfconst
+import elftools.elf.elffile as elffile
+import io
+
+
+# Default section names and their build document keys to look in the ELF file.
+# These are supposed to always be available.
+DEFAULT_ELF_SECTIONS = [
+    (".bss", "vmlinux_bss_size"),
+    (".text", "vmlinux_text_size")
+]
+
+# Write/Alloc & Alloc constant we need to check for in the ELF sections.
+ELF_WA_FLAG = elfconst.SH_FLAGS.SHF_WRITE | elfconst.SH_FLAGS.SHF_ALLOC
+ELF_A_FLAG = elfconst.SH_FLAGS.SHF_ALLOC
+
+
+def calculate_data_size(elf_file):
+    """Loop through the ELF file sections and compute the .data size.
+
+    It will look for all SHT_PROGBITS type sections that have the
+    SHF_ALLOC or SHF_ALLOC+SHF_WRITE flag set and sum their sizes.
+
+    :param elf_file: The open ELF file.
+    :return The .data size or 0.
+    """
+    data_size = 0
+
+    for section in elf_file.iter_sections():
+        elf_flags = section["sh_flags"]
+        if all([section["sh_type"] == "SHT_PROGBITS",
+                any([elf_flags == ELF_WA_FLAG, elf_flags == ELF_A_FLAG])]):
+            data_size += section["sh_size"]
+
+    return data_size
+
+
+def elfread(path):
+    """Read a vmlinux file and extract some info from it.
+
+    Update the build document in place.
+
+    Info extracted:
+        0. Size of the .text section.
+        1. Size of the .data section.
+        2. Size of the .bss section.
+
+    :param path: The path to the vmlinux file.
+    :type path: str
+    :return A dictionary with the extracted values.
+    """
+    extracted = {}
+
+    if os.path.isfile(path):
+        with io.open(path, mode="rb") as vmlinux_strm:
+            elf_file = elffile.ELFFile(vmlinux_strm)
+
+            for elf_sect in DEFAULT_ELF_SECTIONS:
+                sect = elf_file.get_section_by_name(elf_sect[0])
+                if sect:
+                    extracted[elf_sect[1]] = sect["sh_size"]
+
+            data_sect = elf_file.get_section_by_name(".data")
+            if data_sect:
+                extracted["vmlinux_data_size"] = data_sect["sh_size"]
+            else:
+                extracted["vmlinux_data_size"] = \
+                    calculate_data_size(elf_file)
+
+    return extracted
 
 # sys.path.append(os.path.abspath("/kernelci-core"))
 # import kernelci.build as kci_build
@@ -409,7 +479,7 @@ def build_kernel(b_env, kdir, arch, defconfig=None, jopt=None,
 
     vmlinux_file = os.path.join(output_path, 'vmlinux')
     if os.path.isfile(vmlinux_file):
-        vmlinux_meta = kernelci.elf.read(vmlinux_file)
+        vmlinux_meta = elfread(vmlinux_file)
         bmeta.update(vmlinux_meta)
         bmeta['vmlinux_file_size'] = os.stat(vmlinux_file).st_size
 
@@ -429,5 +499,6 @@ if __name__ == "__main__":
     download_kernel(kver)
     extraction_path = extract_kernel(kver)
 
-    build_kernel(b_env=b_env, arch=arch, kdir=extraction_path, defconfig='defconfig')
+    build_kernel(b_env=b_env, arch=arch, kdir=extraction_path, defconfig=config)
+
     shutil.rmtree(extraction_path)
